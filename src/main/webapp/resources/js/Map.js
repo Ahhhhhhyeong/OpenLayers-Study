74ff837ddf2bb2b8f3ba4ae8ffa6e5527d84417c
Ahhhhhhyeong <dkdud081@gmail.com>
2023-02-23 16:35:27 +0900

    err change view

--- src/main/webapp/resources/js/Map.js
+++ src/main/webapp/resources/js/Map.js
@@ -1,18 +1,38 @@
+	// ScaleLine Unit Option Select
 	var scaleLineControl = new ol.control.ScaleLine();
-
+	var projectionSelect = document.getElementById('projection');
+	projectionSelect.addEventListener('change', function(event) {
+		scaleLineControl = new ol.control.ScaleLine({units: event.target.value});
+		$(".ol-scale-line-inner").css("display", "none");
+		map.addControl(scaleLineControl);
+	});
+		
+	
 	// Mouse Position Control
 	var mousePositionControl = new ol.control.MousePosition({
         coordinateFormat: function(coord){
         	var out = ol.coordinate.toStringHDMS(coord, 1);
-        	console.log(out);
         	return out;
         },
         projection: 'EPSG:4326',
         className: 'custom-mouse-position',
-        //target: document.getElementById('mouseCoordinate'),
         undefinedHTML: '&nbsp;'
     });	
 	
+	
+	// Change View	
+	function projectionOnclick(val){
+		var projectionN = 'EPSG:' + val;
+		var projectionF = map.getView().getProjection().hb;
+		console.log(projectionF);
+		map.setView(new ol.View({ projection: projectionN,
+						    	  center: ol.proj.transform([127.6, 37.8], projectionF, projectionN),
+						    	  Zoom: 7 
+						    	}));
+	};				 
+	
+	
+	// Open View
     var map = new ol.Map({
         layers: [
           new ol.layer.Tile({
@@ -35,24 +55,4 @@
         })
       });
     
-	// Change View	
-	function projectionOnclick(val){
-		var projectionN = 'EPSG:' + val;
-		console.log(projectionN);
-		mapView = new ol.View({
-			projection: projectionN,
-	    	center: ol.proj.transform([127.6, 37.8], 'EPSG:4326', projectionN ),
-	    	Zoom: 7
-		});	
-		map.setView(mapView);
-	};			
-	
-	
-	
-	// Scale Unit Option Select
-	var projectionSelect = document.getElementById('projection');
-	projectionSelect.addEventListener('change', function(event) {
-		console.log(event)
-		mousePositionControl.setProjection(ol.proj.get(event.target.value));		
-	});
-	
+
