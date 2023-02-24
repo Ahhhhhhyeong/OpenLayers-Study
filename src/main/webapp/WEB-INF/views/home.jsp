4bb566535f98561d388ee07cd9e83a297b9423f6
Ahhhhhhyeong <dkdud081@gmail.com>
2023-02-24 17:58:35 +0900

    Open Layers fin

--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -3,45 +3,17 @@
 <%@ page contentType="text/html; charset=UTF8" pageEncoding="UTF-8" %>
 <html>
 <head>
-	<title>OpenLayers Study</title>
-	<link rel="stylesheet" href="https://openlayers.org/en/v3.20.1/css/ol.css" type="text/css">
+	<title>OpenLayers Study</title>	
     <!-- jQuery -->
 	<script type="text/javascript" src="${pageContext.request.contextPath}/js/jquery/jquery-1.11.1.min.js"></script>
 	<script type="text/javascript" src="${pageContext.request.contextPath}/js/jquery/jquery-3.0.0.min.js"></script>
 	<script type="text/javascript" src="${pageContext.request.contextPath}/js/jquery/jquery-ui.min.js"></script>
-    
-    <script type="text/javascript" src="https://cdn.polyfill.io/v2/polyfill.min.js?features=requestAnimationFrame,Element.prototype.classList,URL"></script>
-    <script type="text/javascript" src="https://openlayers.org/en/v3.20.1/build/ol.js"></script>	
-    
-		
-	<style>
-		#map{	
-			position:relative		
-			height: 100%;
-			width: 80%;
-			float: right;			
-		}	
-			
-		#map .custom-mouse-position {
-			position: absolute;
-			top: 0; left:84%;
-			z-index: 0
-		}
-		
-		.left-menu{		
-			width: 20%;	
-			float: left;
-		}
-		
-		.ol-zoom{
-			left: unset;
-			right: 10;
-			top: 25
-		}
-		
-		
-	</style>
-</head>
+    <!-- OpenLayers -->
+    <script src="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.15.1/build/ol.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.15.1/css/ol.css" type="text/css">
+	<!-- css -->
+	<link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/css/main.css">	
+</head> 
 <body>
 	<div class="left-menu">
 		<h2>Layer</h2>
@@ -61,7 +33,10 @@
 	</div>	
 	
 	<div id="map" class="map" tabindex="0"></div>
-	
+	<div id="popup" class="ol-popup">
+      <a href="#" id="popup-closer" class="ol-popup-closer"></a>
+      <div id="popup-content"></div>
+    </div>
 	<script src="${pageContext.request.contextPath}/js/Map.js"></script>
 </body>
 </html>
--- src/main/webapp/resources/js/Map.js
+++ src/main/webapp/resources/js/Map.js
@@ -22,17 +22,48 @@
 	
 	
 	// Change View		 
-	   function projectionOnclick(val){
-		      var projectionN = 'EPSG:' + val;
+	function projectionOnclick(val){
+		  var projectionN = 'EPSG:' + val;
 		      
-		      var remapview = new ol.View({
-		         projection: projectionN,
-		         center: ol.proj.transform([127.6, 37.8], 'EPSG:4326', projectionN),
-		         zoom: 7
-		      });
+		  var remapview = new ol.View({
+									         projection: projectionN,
+									         center: ol.proj.transform([127.6, 37.8], 'EPSG:4326', projectionN),
+									         zoom: 7
+		   					         });
 		      
-		      map.setView(remapview);
-		   };   
+	      map.setView(remapview);
+	};   
+	
+    // popup
+    /**
+     * Elements that make up the popup.
+     */
+    var container = document.getElementById('popup');
+    var content = document.getElementById('popup-content');
+    var closer = document.getElementById('popup-closer');
+    
+    /**
+     * Create an overlay to anchor the popup to the map.
+     */
+    var overlay = new ol.Overlay({
+    	element: container,
+    	autoPan: {
+    		animation: {
+    			duration: 250,
+    		},
+    	},
+    });
+    
+    /**
+     * Add a click handler to hide the popup.
+     * @return {boolean} Don't follow the href.
+     */
+    closer.onclick = function () {
+      overlay.setPosition(undefined);
+      closer.blur();
+      return false;
+    };
+   
 	
 	// Open View
     var map = new ol.Map({
@@ -54,7 +85,55 @@
         	projection: 'EPSG:3857',
           center: ol.proj.transform([127.6, 37.8], 'EPSG:4326', 'EPSG:3857'),
           zoom: 7
-        })
+        }),
+        overlays: [overlay],
       });
     
-
+    
+    // show portLayer
+    var portLayer = new ol.layer.Tile({
+    	source: new ol.source.TileWMS({
+    		url: "",
+    		params: {
+    			
+    		} 
+    	})
+    });
+    map.addLayer(portLayer);
+    
+    // show data with Overlay
+    map.on('singleclick', function (evt) {
+    	  var coordinate = evt.coordinate;
+    	  var viewResolution = map.getView().getResolution(); 
+    	  var viewProjection = map.getView().getProjection().et;
+    	  
+    	  var url = portLayer.getSource().getFeatureInfoUrl(
+    	    coordinate,
+    	    viewResolution,
+    	    viewProjection,
+    	    {'INFO_FORMAT': 'application/json'}
+    	  );
+    	  
+    	  $.ajax({
+    		 type: 'GET',
+    		 url: url,
+    		 datatype: 'json',
+    		 contentType: false,
+    		 async: false
+    	  }).done(function(resp){   		  
+    		  if(resp.features[0]){
+    			  var strHtml="";
+    			   var data = resp.features[0].properties;    			   
+    			   for (variable in data) {
+    					strHtml+=variable 
+    					strHtml+= " : "
+    					strHtml+= data[variable];
+    					strHtml+="<br>"
+    				} 			  
+    			  content.innerHTML =  strHtml;
+        		  overlay.setPosition(coordinate);
+    		  }
+    		  
+    	  });
+    });
+    
